f0b0d1a0fc5b52e220fc662626ef17412b02d9fb
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-04-18 23:02:26 +0530

    Time: 26 ms (57.80%) | Memory: 45.2 MB (90.43%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,25 +1,16 @@
 class Solution {
-    private int lcs(String s1,String s2){
-        int n = s1.length();
+    public int minInsertions(String s) {
+        int n = s.length();
         int[][] dp = new int[n+1][n+1];
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                if(s.charAt(i-1)==s.charAt(n-j)){
                     dp[i][j]=1+dp[i-1][j-1];
                 }else{
                     dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        return dp[n][n];
-    }
-    private int lps(String s){
-        StringBuilder sb = new StringBuilder(s);
-
-        return lcs(s,sb.reverse().toString());
-    }
-    public int minInsertions(String s) {
-        int n = s.length();
-        return n-lps(s); 
+        return n-dp[n][n]; 
     }
 }
