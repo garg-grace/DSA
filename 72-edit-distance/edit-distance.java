6b97a3b3a326115af598fc5160aca3a76bc007ac
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-04-20 16:50:21 +0530

    Time: 5 ms (63.73%) | Memory: 45.3 MB (19.48%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,17 +1,20 @@
 class Solution {
-    private int f(int i,int j,String s1,String s2,int[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)) return dp[i][j]=f(i-1,j-1,s1,s2,dp);
-                                    //deletion-------------insertion----------replace
-        return dp[i][j]=1+Math.min(f(i-1,j,s1,s2,dp),Math.min(f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp)));
-    }
     public int minDistance(String word1, String word2) {
         int n=word1.length(), m =word2.length();
-        int[][] dp = new int[n][m];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int j=0;j<=m;j++) dp[0][j]=j;
 
-        return f(n-1,m-1,word1,word2,dp);        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
+        }
+
+        return dp[n][m];        
     }
 }
