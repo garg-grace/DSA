27f335fa9bbd1d40a02055e3cc67729712ad9892
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-04-20 17:40:42 +0530

    Time: 23 ms (42.54%) | Memory: 44.8 MB (65.81%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,35 +1,36 @@
 class Solution {
-    private boolean f(int i,int j,String s,String p,int[][] dp){
-        if(i<0 && j<0) return true;
-        if(j<0 && i>=0) return false;
-        if(i<0 && j>=0){
-            for(int jj=0;jj<=j;jj++){
-                if(p.charAt(jj)!='*') return false;
-            }
-            return true;
-        }
-
-        if(dp[i][j]!=-1) return dp[i][j]==1;
-
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
-            boolean res = f(i-1,j-1,s,p,dp);
-            dp[i][j]=(res==true)?1:0;
-            return res;
-        }else if(p.charAt(j)=='*'){
-            boolean res = f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp);
-            dp[i][j]=(res==true)?1:0;
-            return res;
-        }
-
-        dp[i][j]=0;
-        return false;
-    }
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        int[][] dp = new int[n][m];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+        boolean[][] dp = new boolean[n+1][m+1];
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++){
+            dp[i][0]=false;
+        }
+        for(int j=1;j<=m;j++){
+            boolean flag=true;
+            for(int jj=1;jj<=j;jj++){
+                if(p.charAt(jj-1)!='*'){
+                    flag=false;
+                    break;
+                }
+            }
+            dp[0][j]=flag;
+        }
 
-        return f(n-1,m-1,s,p,dp);        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }else if(p.charAt(j-1)=='*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        
+
+        return dp[n][m];        
     }
 }
