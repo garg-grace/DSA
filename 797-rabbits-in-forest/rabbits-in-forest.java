348ae6688afa663554905d07d2454bf465234c69
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-04-20 23:42:29 +0530

    Time: 3 ms (82.48%) | Memory: 42.1 MB (90.69%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.java
+++ 797-rabbits-in-forest/rabbits-in-forest.java
@@ -1,15 +1,23 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
+        Map<Integer,Integer> mp = new HashMap<>();
+
         for(int num:answers){
             mp.put(num,mp.getOrDefault(num,0)+1);
         }
-
         int ans=0;
-        for(Map.Entry<Integer,Integer> it : mp.entrySet()){
-            int color = it.getKey();
-            int count = it.getValue();
-            ans+=Math.ceil((double)count/(color+1))*(color+1);
+        for(Map.Entry<Integer,Integer> it:mp.entrySet()){
+            int color=it.getKey();
+            int count=it.getValue();
+
+            if(count>color+1){
+                int freq = count/(color+1);
+                ans+=(freq*(color+1));
+                int rem = count%(color+1);
+                if(rem!=0) ans+=(color+1);
+            }else{
+                ans+=color+1;
+            }
         }
         return ans;
     }
