642c56848d4e9006dcdbc4664ae56d9e53d19387
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-04-21 20:19:43 +0530

    Time: 22 ms (49.47%) | Memory: 63.5 MB (38.97%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,20 +1,18 @@
 class Solution {
-    private int f(int ind,int trans,int n,int[] prices,int[][] dp){
-        if(ind==n || trans==4) return 0;
-        if(dp[ind][trans]!=-1) return dp[ind][trans];
-
-        if(trans%2==0){
-            return dp[ind][trans]=Math.max(-prices[ind]+f(ind+1,trans+1,n,prices,dp),
-                                        f(ind+1,trans,n,prices,dp));
-        }
-
-        return dp[ind][trans]=Math.max(prices[ind]+f(ind+1,trans+1,n,prices,dp),
-                                    f(ind+1,trans,n,prices,dp));
-    }
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][4];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        return f(0,0,n,prices,dp);
+        int[][] dp = new int[n+1][5];
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int trans=3;trans>=0;trans--){
+                if(trans%2==0){
+                    dp[ind][trans]=Math.max(-prices[ind]+dp[ind+1][trans+1],dp[ind+1][trans]);
+                }else{
+                    dp[ind][trans]=Math.max(prices[ind]+dp[ind+1][trans+1],dp[ind+1][trans]);
+                }
+            }
+        }
+        
+        return dp[0][0];
     }
 }
