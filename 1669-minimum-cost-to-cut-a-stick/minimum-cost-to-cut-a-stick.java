1ef7dbc1017a9feb17848baf9400a29aeae98a18
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-04-30 23:08:09 +0530

    Time: 22 ms (62.23%) | Memory: 43 MB (18.38%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,15 +1,4 @@
 class Solution {
-    private int f(int i,int j,List<Integer> cuts,int[][] dp){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int mini=Integer.MAX_VALUE;
-        for(int ind=i;ind<=j;ind++){
-            int ans = cuts.get(j+1)-cuts.get(i-1)+f(i,ind-1,cuts,dp)+f(ind+1,j,cuts,dp);
-
-            if(ans<mini) mini=ans;
-        }
-        return dp[i][j]=mini;
-    }
     public int minCost(int n, int[] arr) {
         List<Integer> cuts = new ArrayList<>();
         for(int a:arr) cuts.add(a);
@@ -17,9 +6,20 @@ class Solution {
         cuts.add(n);
         Collections.sort(cuts);
         int c = cuts.size();
-        int[][] dp = new int[c+1][c+1];
-        for(int i=0;i<=c;i++) Arrays.fill(dp[i],-1);
+        int[][] dp = new int[c+2][c+2];
+        for(int i=c;i>=1;i--){
+            for(int j=1;j<=c-2;j++){
+                if(i>j) continue;
+                int mini=Integer.MAX_VALUE;
+                for(int ind=i;ind<=j;ind++){
+                    int ans=cuts.get(j+1)-cuts.get(i-1)+dp[i][ind-1]+dp[ind+1][j];
 
-        return f(1,c-2,cuts,dp);   
+                    if(ans<mini) mini=ans;
+                }
+                dp[i][j]=mini;
+            }
+        }
+
+        return dp[1][c-2];   
     }
 }
