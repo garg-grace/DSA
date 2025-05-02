323f6368b4ef3d633ef312535fc2e258162350fe
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-05-02 17:32:03 +0530

    Time: 2475 ms (35.16%) | Memory: 41.5 MB (81.99%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -8,24 +8,23 @@ class Solution {
         }
         return true;
     }
-    private int f(int i,int n,String s,int[] dp){
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int mini = Integer.MAX_VALUE;
-        for(int j=i;j<n;j++){
-            if(isPalindrome(i,j,s)){
-                int cost =1+f(j+1,n,s,dp);
-
-                if(cost<mini) mini=cost;
-            }
-        }
-        return dp[i]=mini;
-    }
     public int minCut(String s) {
         int n = s.length();
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
+        int[] dp = new int[n+1];
+        dp[n]=0;
 
-        return f(0,n,s,dp)-1;        
+        for(int i=n-1;i>=0;i--){
+            int mini = Integer.MAX_VALUE;
+            for(int j=i;j<n;j++){
+                if(isPalindrome(i,j,s)){
+                    int cost = 1+dp[j+1];
+
+                    if(cost<mini) mini=cost;
+                }
+            }
+            dp[i]=mini;
+        }
+
+        return dp[0]-1;        
     }
 }
