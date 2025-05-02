d6c21445467249bdebd55943fb06c7350ffbca77
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-05-02 15:15:48 +0530

    Time: 26 ms (38.87%) | Memory: 51.9 MB (12.45%) - LeetSync

--- 868-push-dominoes/push-dominoes.java
+++ 868-push-dominoes/push-dominoes.java
@@ -1,29 +1,38 @@
+class Pair{
+    int i;
+    char d;
+    public Pair(int i,char d){
+        this.i=i;
+        this.d=d;
+    }
+}
 class Solution {
-    public String pushDominoes(String s) {
-        s = "L" + s + "R";
-        StringBuilder res = new StringBuilder();
-        int prev = 0;
-        for (int curr = 1; curr < s.length(); ++curr) {
-            if (s.charAt(curr) == '.') continue;
-            int span = curr - prev - 1;
-            if (prev > 0)
-                res.append(s.charAt(prev));
-            if (s.charAt(prev) == s.charAt(curr)) {
-                for (int i = 0; i < span; ++i)
-                    res.append(s.charAt(prev));
-            } else if (s.charAt(prev) == 'L' && s.charAt(curr) == 'R') {
-                for (int i = 0; i < span; ++i)
-                    res.append('.');
-            } else {
-                for (int i = 0; i < span / 2; ++i)
-                    res.append('R');
-                if (span % 2 == 1)
-                    res.append('.');
-                for (int i = 0; i < span / 2; ++i)
-                    res.append('L');
-            }
-            prev = curr;
+    public String pushDominoes(String dominoes) {
+        StringBuilder sb = new StringBuilder(dominoes);
+        Queue<Pair> q = new LinkedList<>();
+
+        for(int i=0;i<dominoes.length();i++){
+            if(dominoes.charAt(i)!='.') q.add(new Pair(i,dominoes.charAt(i)));
         }
-        return res.toString();
+
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int i=p.i;
+            char d=p.d;
+
+            if(d=='L' && i>0 && sb.charAt(i-1)=='.'){
+                q.add(new Pair(i-1,'L'));
+                sb.setCharAt(i-1,'L');
+            }else if(d=='R'){
+                if((i+1)<sb.length() && sb.charAt(i+1)=='.'){
+                    if((i+2)<sb.length() && sb.charAt(i+2)=='L') q.remove();
+                    else{
+                        q.add(new Pair(i+1,'R'));
+                        sb.setCharAt(i+1,'R');
+                    }
+                }
+            }
+        }
+        return sb.toString();        
     }
 }
