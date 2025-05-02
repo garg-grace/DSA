de51229ca21a9c06594aae0014ed39966ef09cde
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-05-02 20:17:00 +0530

    Time: 6 ms (93.97%) | Memory: 42.1 MB (79.24%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,23 +1,20 @@
 class Solution {
-    private int f(int i,int n,int k,int[] arr,int[] dp){
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-
-        int len=0,maxi=Integer.MIN_VALUE,maxAns=Integer.MIN_VALUE;
-        for(int j=i;j<Math.min(n,i+k);j++){
-            len++;
-            maxi=Math.max(maxi,arr[j]);
-            int sum = len*maxi+f(j+1,n,k,arr,dp);
-
-            maxAns=Math.max(maxAns,sum);
-        }
-        return dp[i]=maxAns;
-    }
-    public int maxSumAfterPartitioning(int[] arr, int k) {
+   public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
+        int[] dp = new int[n+1];
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int len=0,maxi=Integer.MIN_VALUE,maxAns=Integer.MIN_VALUE;
+            for(int j=i;j<Math.min(n,i+k);j++){
+                len++;
+                if(arr[j]>maxi) maxi=arr[j];
+                int sum = len*maxi+dp[j+1];
 
-        return f(0,n,k,arr,dp);        
+                if(sum>maxAns) maxAns=sum;
+            }
+            dp[i]=maxAns;
+        }
+
+        return dp[0];        
     }
 }
