a9c48416f1abbbc972829e2aedfa71525b3cd464
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-05-04 10:38:27 +0530

    Time: 2 ms (98.22%) | Memory: 52.9 MB (58.27%) - LeetSync

---
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] mpp = new int[100];
+        for(int[] d:dominoes){
+            if(d[0]>d[1]) mpp[d[0]*10+d[1]]++;
+            else mpp[d[1]*10+d[0]]++;
+        }
+
+        int cnt=0;
+        for(int freq:mpp){
+            cnt+=(freq-1)*freq/2;
+        }
+        return cnt;        
+    }
+}
