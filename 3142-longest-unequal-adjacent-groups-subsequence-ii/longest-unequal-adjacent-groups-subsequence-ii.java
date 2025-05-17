9ee0c1a48ff818cb5441df6810893ebc669d1d53
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-05-17 11:14:28 +0530

    Time: 40 ms (89.89%) | Memory: 45.1 MB (40.45%) - LeetSync

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.java
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.java
@@ -1,44 +1,47 @@
 class Solution {
-    public boolean differByOneChar(String word1, String word2) {
-        if (word1.length() != word2.length()) return false;
-        int diffCount = 0;
-        for (int i = 0; i < word1.length(); i++)
-            if (word1.charAt(i) != word2.charAt(i))
-                diffCount++;
-        return diffCount == 1;
+    public boolean checkHammingCondition(String str1,String str2){
+        if(str1.length()!=str2.length()) return false;
+
+        int cnt=0;
+        for(int i=0;i<str1.length();i++){
+            if(str1.charAt(i)!=str2.charAt(i)) cnt++;
+            if(cnt>1) return false;
+        }
+        return cnt==1;
     }
-
     public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
-        int n = groups.length;
+        int n = words.length;
         int[] dp = new int[n];
-        int[] parent = new int[n];
-        Arrays.fill(dp, 1);
-        Arrays.fill(parent, -1);
-        int maxi = 0;
+        Arrays.fill(dp,1);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (groups[i] != groups[j] &&
-                    differByOneChar(words[i], words[j]) &&
-                    dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
+        int[] parent = new int[n];
+        Arrays.fill(parent,-1);     
+        int maxi=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(groups[i]!=groups[j] && checkHammingCondition(words[i],words[j])){
+                    if(dp[i]<1+dp[j]){
+                        dp[i]=1+dp[j];
+                        parent[i]=j;
+                    }
                 }
             }
-            if (dp[i] > maxi) maxi = dp[i];
+            if(dp[i]>maxi) maxi=dp[i];
         }
 
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (dp[i] == maxi) {
-                while (i != -1) {
-                    result.add(words[i]);
-                    i = parent[i];
+        List<String> res = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(dp[i]==maxi){
+                while(i!=-1){
+                    res.add(words[i]);
+                    i=parent[i];
                 }
                 break;
             }
         }
-        Collections.reverse(result);
-        return result;
+        
+        Collections.reverse(res);
+        return res;
     }
 }
