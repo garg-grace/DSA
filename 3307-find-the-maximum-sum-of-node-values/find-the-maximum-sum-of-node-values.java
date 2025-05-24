8d3c857e883653a76bcdf475d71f5d34c09257b0
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-05-24 16:24:52 +0530

    Time: 27 ms (10.53%) | Memory: 51.8 MB (98.68%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.java
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.java
@@ -1,12 +1,22 @@
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        long[] dp = {0, Long.MIN_VALUE};
-        for (int n : nums) {
-            long cnt0 = dp[0] + n, cnt1 = dp[1] + n;
-            int x = n ^ k;
-            dp = new long[]{ Math.max(cnt0, dp[1] + x), 
-                             Math.max(cnt1, dp[0] + x) };
+        int n = nums.length;
+        Integer[] delta = new Integer[n];
+        long sum = 0;
+        for(int i=0;i<n;i++){
+            delta[i] = (nums[i]^k) - nums[i];
+            sum+=nums[i];
         }
-        return dp[0];
+        Arrays.sort(delta,(a,b)->b-a);
+
+        for(int i=0;i<n;i+=2){
+            if(i==n-1) break;
+            int temp = delta[i]+delta[i+1];
+
+            if(temp<=0) break;
+
+            sum+=temp; 
+        }
+        return sum;        
     }
 }
