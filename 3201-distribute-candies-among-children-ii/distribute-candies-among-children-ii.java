7610d6910cf94be646c4b7b540a6265288f9d2b2
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-06-01 22:55:53 +0530

    Time: 23 ms (33.82%) | Memory: 41 MB (47.06%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.java
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.java
@@ -1,10 +1,14 @@
 class Solution {
     public long distributeCandies(int n, int limit) {
-        long res = 0;
-        for (int i = 0; i <= Math.min(limit, n); i++) {
-            if (n - i <= 2 * limit)
-                res += Math.min(n - i, limit) - Math.max(0, n - i - limit) + 1;
+        long minCh1 = Math.max(0,n-2*limit);
+        long maxCh1 = Math.min(n,limit);
+        long ways = 0;
+        for(long i=minCh1;i<=maxCh1;i++){
+            long N = n-i;
+            long minCh2 = Math.max(0,N-limit);
+            long maxCh2 = Math.min(N,limit);
+            ways += (maxCh2-minCh2+1);
         }
-        return res;
+        return ways;        
     }
 }
