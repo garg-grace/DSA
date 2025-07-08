3ab6df5e1cfe70feaff0464692d1563deb80c897
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-07-08 23:48:02 +0530

    Time: 119 ms (32.69%) | Memory: 138.6 MB (16.83%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,31 +1,39 @@
 class Solution {
+    private int solve(int[][] events,int i,int n,int k,int[][] dp){
+        if(i>=n || k<=0) return 0;
+
+        if(dp[i][k]!=-1) return dp[i][k];
+
+        int start = events[i][0];
+        int end = events[i][1];
+        int val = events[i][2];
+
+        int skip = solve(events,i+1,n,k,dp);
+        int j = binarySearch(events,end+1,i+1);
+        int take = val+solve(events,j,n,k-1,dp);
+
+        return dp[i][k]=Math.max(skip,take);
+    }
     public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, (a, b) -> a[1] - b[1]); 
+        Arrays.sort(events, (a, b) -> a[0] - b[0]); 
         int n = events.length;
 
         int[][] dp = new int[n + 1][k + 1];
+        for(int i=0;i<=n;i++) Arrays.fill(dp[i],-1);
 
-        for (int i = 1; i <= n; i++) {
-            int[] event = events[i - 1];
-            int prev = binarySearch(events, event[0]);
-
-            for (int j = 1; j <= k; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[prev + 1][j - 1] + event[2]);
-            }
-        }
-        return dp[n][k];
+        return solve(events,0,n,k,dp);
     }
-    private int binarySearch(int[][] events, int currentStart) {
-        int left = 0, right = events.length - 1;
-        int result = -1;
+    private int binarySearch(int[][] events, int lastEnd,int left) {
+        int right = events.length - 1;
+        int result = events.length;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (events[mid][1] < currentStart) {
+            if (events[mid][0] >= lastEnd) {
                 result = mid;
-                left = mid + 1;
-            } else {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         return result;
