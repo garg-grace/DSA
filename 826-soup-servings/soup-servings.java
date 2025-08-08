6030bbc0dbc5878daf1ece56a63ffee267e1e479
Grace garg <68893823+garg-grace@users.noreply.github.com>
2025-08-08 09:15:37 +0530

    Time: 1 ms (89.22%) | Memory: 41 MB (98.80%) - LeetSync

---
+++ 826-soup-servings/soup-servings.java
@@ -0,0 +1,28 @@
+class Solution {
+    private Double[][] dp;
+
+    public double soupServings(int n) {
+        if (n > 5000) return 1.0;
+
+        int m = (int)Math.ceil(n / 25.0);
+        dp = new Double[m+1][m+1];
+        return findProb(m, m);
+    }
+
+    private double findProb(int a, int b) {
+        if (a <= 0 && b <= 0) return 0.5;
+        if (a <= 0) return 1.0;
+        if (b <= 0) return 0.0;
+
+        if (dp[a][b] != null) return dp[a][b];
+
+        dp[a][b] = 0.25 * (
+            findProb(a-4, b) +        
+            findProb(a-3, b-1) +        
+            findProb(a-2, b-2) +        
+            findProb(a-1, b-3)          
+        );
+
+        return dp[a][b];
+    }
+}
