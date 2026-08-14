08710984a051e89c47480f2008ace4f1f378fbb5
Grace garg <68893823+garg-grace@users.noreply.github.com>
2026-08-14 18:03:00 +0530

    Time: 1 ms (99.89%) | Memory: 56 MB (6.75%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ele = nums[0];
+        int cnt = 1;
+
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i]==ele) {
+                cnt++;
+            } else {
+                cnt--;
+                if(cnt<=0){
+                    ele=nums[i];
+                    cnt=1;
+                } 
+            }
+        }
+        return ele;
+    }
+}
